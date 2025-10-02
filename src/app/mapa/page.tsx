3fca26b07f2ecd82d4808932e81a9ba0f8ec6d6b
Pablo Dias <pabllosoarez@gmail.com>
2025-10-02 11:01:26 -0300

    feat: implement location permission prompts with animated transitions; enhance NearbyHeader and LocationDenied components

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -1,16 +1,22 @@
 'use client'
 
+import { useState } from 'react'
 import { css } from '../../../styled-system/css'
 import { ArrowClockwiseIcon } from '@phosphor-icons/react/dist/ssr'
 
 import { Button } from '@/components/Button'
 import { SearchBar } from '@/components/Navbar/SearchBar'
 import { Paragraph } from '@/components/Typography/Paragraph'
-import { GpsFixIcon } from '@phosphor-icons/react'
+import { GpsFixIcon, GpsSlashIcon } from '@phosphor-icons/react'
 import { contentContainer, searchBarContainer } from './styles'
 import { ProgressiveBlur } from '@/components/ProgressiveBlur'
+import { AnimatePresence, motion } from 'motion/react'
 
 export default function Page() {
+    const [userAllowedLocation, setUserAllowedLocation] = useState<
+        null | boolean
+    >(null)
+
     return (
         <main>
             <section className={searchBarContainer}>
@@ -18,25 +24,100 @@ export default function Page() {
                 <ProgressiveBlur />
             </section>
             <div className={contentContainer}>
-                <header className={headerStyles}>
-                    <Paragraph bolder>Próximos de você</Paragraph>
-                    <div>
-                        <Button iconButton variant="subtle">
-                            <ArrowClockwiseIcon weight="bold" />
-                        </Button>
-                    </div>
-                </header>
-                <Paragraph>
-                    Autorize o uso da localização para encontrar unidades de
-                    saúde próximas a você.
-                </Paragraph>
-                <Button>
-                    <GpsFixIcon /> Autorizar localização
-                </Button>
+                <AnimatePresence initial={false}>
+                    {userAllowedLocation === null && (
+                        <LocationPrompt
+                            setUserAllowedLocation={setUserAllowedLocation}
+                        />
+                    )}
+                    {userAllowedLocation === true && <NearbyHeader />}
+                    {userAllowedLocation === false && <LocationDenied />}
+                </AnimatePresence>
             </div>
         </main>
     )
 }
+
+const LocationPrompt = ({
+    setUserAllowedLocation,
+}: {
+    setUserAllowedLocation: React.Dispatch<React.SetStateAction<null | boolean>>
+}) => (
+    <motion.div
+        key={1}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+        }}
+    >
+        <Paragraph>
+            Autorize o uso da localização para encontrar unidades de saúde
+            próximas a você.
+        </Paragraph>
+        <Button onClick={() => setUserAllowedLocation(true)}>
+            <GpsFixIcon /> Autorizar localização
+        </Button>
+        <Button variant="text" onClick={() => setUserAllowedLocation(false)}>
+            <GpsSlashIcon /> Não autorizar localização
+        </Button>
+    </motion.div>
+)
+
+const NearbyHeader = () => (
+    <motion.div
+        key={2}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+        }}
+    >
+        <header className={headerStyles}>
+            <Paragraph bolder size="subheadline">
+                Próximo de você
+            </Paragraph>
+            <div>
+                <Button iconButton variant="subtle">
+                    <ArrowClockwiseIcon weight="bold" />
+                </Button>
+            </div>
+        </header>
+    </motion.div>
+)
+
+const LocationDenied = () => (
+    <motion.div
+        key={3}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+        }}
+    >
+        <Paragraph subtle centered size="subheadline">
+            Você não autorizou o uso da sua localização.
+        </Paragraph>
+    </motion.div>
+)
 
 const headerStyles = css({
     display: 'flex',
