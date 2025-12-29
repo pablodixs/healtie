c8f4f0e6ed1a7d050427af432b9433bbb51e077e
Pablo Dias <pabllosoarez@gmail.com>
2025-12-29 07:42:32 -0300

    feat: add EstablishmentServices interface to define service availability

---
+++ src/interfaces/EstablishmentServices.ts
@@ -0,0 +1,8 @@
+export interface EstablishmentServices {
+    has_surgical_center: boolean
+    has_obstetric_center: boolean
+    has_neonatal_center: boolean
+    has_hospital_care: boolean
+    has_support_service: boolean
+    has_ambulatory_care: boolean
+}
