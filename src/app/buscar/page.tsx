20ebff946f08ec49f5c19970cd4fc8033fb15ead
Pablo Dias <pabllosoarez@gmail.com>
2026-01-03 17:21:29 -0300

    feat: enhance search suggestions with improved animations and input focus handling

--- src/app/buscar/page.tsx
+++ src/app/buscar/page.tsx
@@ -60,16 +60,21 @@ interface SuggestionResponse {
 
 const suggestionsContainer = {
     visible: {
+        opacity: 1,
+        filter: 'blur(0px)',
+        y: 0,
         transition: {
             when: 'beforeChildren',
             delayChildren: stagger(0.1),
-            type: 'spring',
-            bounce: 0,
         },
     },
     hidden: {
+        opacity: 0,
+        filter: 'blur(2px)',
+        y: -10,
         transition: {
             when: 'afterChildren',
+            delayChildren: stagger(0.1),
         },
     },
 } as const
@@ -81,7 +86,7 @@ const suggestionsOptions = {
         filter: 'blur(0px)',
     },
     hidden: {
-        y: 20,
+        y: '-100%',
         opacity: 0,
         filter: 'blur(2px)',
     },
@@ -110,6 +115,9 @@ export default function Page() {
     const query = searchParams.get('q') || ''
     const rawFilterParam = searchParams.get('filter')
     const filterParam = isAllowedFilter(rawFilterParam) ? rawFilterParam : null
+    const [inputFocused, setInputFocused] = useState<boolean | undefined>(
+        undefined
+    )
 
     useEffect(() => {
         setLocalQuery(query)
@@ -154,7 +162,7 @@ export default function Page() {
             clearTimeout(timeoutId)
             controller.abort()
         }
-    }, [localQuery])
+    }, [localQuery, coords])
 
     const handleFilterChange = useCallback(
         (
@@ -181,8 +189,6 @@ export default function Page() {
             : null,
         fetcher
     )
-
-    console.log(data)
 
     const handleSearch = () => {
         router.replace(
@@ -215,15 +221,136 @@ export default function Page() {
             >
                 <Heading>Buscar</Heading>
             </div>
-            <HeroSearchBar
-                showFilterOptions
-                filterValue={establishmentFilter}
-                onFilterChange={handleFilterChange}
-                autoFocus
-                value={localQuery}
-                onChange={(e) => handleChange(e)}
-                searchAction={handleSearch}
-            />
+            <div className={css({ position: 'relative' })}>
+                <HeroSearchBar
+                    showFilterOptions
+                    filterValue={establishmentFilter}
+                    onFilterChange={handleFilterChange}
+                    autoFocus
+                    value={localQuery}
+                    onChange={(e) => handleChange(e)}
+                    searchAction={handleSearch}
+                    isInputFocused={inputFocused}
+                    onInputFocusChange={() => setInputFocused(!inputFocused)}
+                />
+                <AnimatePresence>
+                    {localQuery && inputFocused && (
+                        <motion.div
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            transition={{
+                                type: 'spring',
+                            }}
+                            variants={suggestionsContainer}
+                            className={css({
+                                position: 'absolute',
+                                top: '4rem',
+                                zIndex: 50,
+                                width: '100%',
+                                maxWidth: '800px',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'flex-start',
+                                gap: '.5rem',
+
+                                _after: {
+                                    content: "''",
+                                    position: 'absolute',
+                                    bottom: '-2rem',
+                                    left: '-2rem',
+                                    right: '-2rem',
+                                    width: '100%',
+                                    height: '100%',
+                                    bg: 'rgba(255, 255, 255, 0.8)',
+                                    zIndex: -1,
+                                    filter: 'blur(20px)',
+                                    backdropBlur: '20px',
+                                },
+
+                                '& button': {
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    gap: '1ch',
+                                    padding: '0.5rem 0.75rem',
+                                    bg: 'white',
+                                    border: '1px solid rgba(0, 0, 0, 0.1)',
+                                    borderRadius: '9999px',
+                                    cursor: 'pointer',
+                                    textWrap: 'nowrap',
+                                    boxShadow: '0 0 20px rgba(0, 0, 0, 0.05)',
+                                },
+                            })}
+                        >
+                            <button
+                                onClick={handleSearch}
+                                className={css({ zIndex: 51 })}
+                            >
+                                <MagnifyingGlassIcon weight="bold" />
+                                <span>
+                                    Buscar por{' '}
+                                    <b
+                                        className={css({
+                                            fontWeight: 500,
+                                        })}
+                                    >
+                                        &quot;{localQuery}&quot;
+                                    </b>
+                                </span>
+                            </button>
+                            <AnimatePresence mode="wait">
+                                {suggestions &&
+                                    suggestions.map((suggestion) => (
+                                        <motion.button
+                                            onClick={() =>
+                                                router.push(
+                                                    `/estabelecimento/${suggestion.cnes}`
+                                                )
+                                            }
+                                            initial="hidden"
+                                            animate="visible"
+                                            exit="exit"
+                                            transition={{
+                                                duration: 0.4,
+                                                type: 'spring',
+                                            }}
+                                            variants={suggestionsOptions}
+                                            key={suggestion.cnes}
+                                        >
+                                            {
+                                                <div
+                                                    className={markerContainer({
+                                                        type: suggestion.type as
+                                                            | 'Hospital Geral'
+                                                            | 'Unidade Básica de Saúde'
+                                                            | 'Unidade de Pronto Atendimento',
+                                                        size: 'xs',
+                                                        square: true,
+                                                    })}
+                                                >
+                                                    {suggestion.type ===
+                                                        'Hospital Geral' && (
+                                                        <HospitalIcon weight="fill" />
+                                                    )}
+                                                    {suggestion.type ===
+                                                        'Unidade Básica de Saúde' && (
+                                                        <FirstAidIcon weight="fill" />
+                                                    )}
+                                                    {suggestion.type ===
+                                                        'Unidade de Pronto Atendimento' && (
+                                                        <AmbulanceIcon weight="fill" />
+                                                    )}
+                                                </div>
+                                            }
+                                            {suggestion.name}
+                                            <ArrowUpRightIcon weight="bold" />
+                                        </motion.button>
+                                    ))}
+                            </AnimatePresence>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
             <AnimatePresence mode="sync">
                 {data && data.length > 0 ? (
                     <motion.div
@@ -489,131 +616,10 @@ export default function Page() {
                         className={css({
                             width: '100%',
                             maxWidth: '800px',
+                            position: 'relative',
                         })}
-                        initial="hidden"
-                        animate="visible"
                     >
-                        <AnimatePresence>
-                            {localQuery && (
-                                <motion.div
-                                    initial={{
-                                        opacity: 0,
-                                        height: 0,
-                                        marginBottom: 0,
-                                    }}
-                                    animate={{
-                                        opacity: 1,
-                                        height: '30rem',
-                                        marginBottom: '1.5rem',
-                                    }}
-                                    exit={{
-                                        opacity: 0,
-                                        height: 0,
-                                        marginBottom: 0,
-                                    }}
-                                    transition={{
-                                        duration: 0.4,
-                                        type: 'spring',
-                                        bounce: 0,
-                                    }}
-                                    variants={suggestionsContainer}
-                                    className={css({
-                                        overflow: 'hidden',
-                                        width: '100%',
-                                        maxWidth: '800px',
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        alignItems: 'flex-start',
-                                        gap: '1rem',
-                                        '& button': {
-                                            display: 'flex',
-                                            alignItems: 'center',
-                                            gap: '1ch',
-                                            padding: '0.25rem 0.5rem',
-                                            bg: 'neutral.100',
-                                            borderRadius: '9999px',
-                                            cursor: 'pointer',
-                                            textWrap: 'nowrap',
-                                        },
-                                    })}
-                                >
-                                    <button onClick={handleSearch}>
-                                        <MagnifyingGlassIcon weight="bold" />
-                                        <span>
-                                            Buscar por{' '}
-                                            <b
-                                                className={css({
-                                                    fontWeight: 500,
-                                                })}
-                                            >
-                                                &quot;{localQuery}&quot;
-                                            </b>
-                                        </span>
-                                    </button>
-                                    <AnimatePresence mode="wait">
-                                        {suggestions &&
-                                            suggestions.map((suggestion) => (
-                                                <motion.button
-                                                    onClick={() =>
-                                                        router.push(
-                                                            `/estabelecimento/${suggestion.cnes}`
-                                                        )
-                                                    }
-                                                    transition={{
-                                                        duration: 0.4,
-                                                        type: 'spring',
-                                                        bounce: 0,
-                                                    }}
-                                                    variants={
-                                                        suggestionsOptions
-                                                    }
-                                                    key={suggestion.cnes}
-                                                >
-                                                    {
-                                                        <div
-                                                            className={markerContainer(
-                                                                {
-                                                                    type: suggestion.type as
-                                                                        | 'Hospital Geral'
-                                                                        | 'Unidade Básica de Saúde'
-                                                                        | 'Unidade de Pronto Atendimento',
-                                                                    size: 'xs',
-                                                                    square: true,
-                                                                }
-                                                            )}
-                                                        >
-                                                            {suggestion.type ===
-                                                                'Hospital Geral' && (
-                                                                <HospitalIcon weight="fill" />
-                                                            )}
-                                                            {suggestion.type ===
-                                                                'Unidade Básica de Saúde' && (
-                                                                <FirstAidIcon weight="fill" />
-                                                            )}
-                                                            {suggestion.type ===
-                                                                'Unidade de Pronto Atendimento' && (
-                                                                <AmbulanceIcon weight="fill" />
-                                                            )}
-                                                        </div>
-                                                    }
-                                                    {suggestion.name}
-                                                    <ArrowUpRightIcon weight="bold" />
-                                                </motion.button>
-                                            ))}
-                                    </AnimatePresence>
-                                </motion.div>
-                            )}
-                        </AnimatePresence>
-                        <motion.div
-                            initial={false}
-                            animate={{
-                                opacity: localQuery ? 0 : 1,
-                                filter: `blur(${localQuery ? 4 : 0}px)`,
-                            }}
-                            transition={{ duration: 0.3 }}
-                        >
-                            <NearEstablishmentsBanner />
-                        </motion.div>
+                        <NearEstablishmentsBanner />
                     </motion.div>
                 )}
             </AnimatePresence>
